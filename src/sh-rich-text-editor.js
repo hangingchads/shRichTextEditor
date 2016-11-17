8b056d6c36b9d0010bdac1feba886c06751cd673
Chad Snelson <csnelson@insidesales.com>
2016-11-17 10:00:22 -0700

    VIS-3726 - Moved quill styles into quill.snow.scss

--- src/sh-rich-text-editor.js
+++ src/sh-rich-text-editor.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import ShCore from 'sh-core';
 import * as _ from 'lodash';
+import './quill.snow.scss';
 import './sh-rich-text-editor.scss';
 
 
